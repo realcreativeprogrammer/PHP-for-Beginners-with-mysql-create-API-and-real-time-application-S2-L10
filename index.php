5e8fd64883107c87374894d4b38a0ccf931579f5
realcreativeprogrammer <realcreativeprogrammer@gmail.com>
2022-06-12 14:19:47 +0300

    first

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$color ='red';
+
+switch($color){
+    case 'red':
+        echo 'my color is red';
+        break;
+    case 'green':
+        echo 'my color is green';
+        break;
+    default:
+    echo 'i dont know that color';
+        break;
+
+}
+
+?>
